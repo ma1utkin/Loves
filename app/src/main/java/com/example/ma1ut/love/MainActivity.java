8a82551f7af6ca03f531fd7d2edf0b0df56adf00
ma1utkin <ma1utko94@mail.ru>
2017-04-27 17:49:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ma1ut/love/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.ma1ut.love;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickToZayaInfo(View view){
+        Intent intent = new Intent(this, ZayaInfo.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/ma1ut/love/ZayaInfo.java
@@ -0,0 +1,13 @@
+package com.example.ma1ut.love;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ZayaInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zaya_info);
+    }
+}
