9a359c044b44d81ee897c85d5788cd10207e996d
ma1utkin <ma1utko94@mail.ru>
2017-04-27 18:39:51 +0300

    update

--- app/src/main/java/com/example/ma1ut/love/MessagesActivity.java
+++ app/src/main/java/com/example/ma1ut/love/MessagesActivity.java
@@ -2,9 +2,10 @@ package com.example.ma1ut.love;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MessagesActivity extends AppCompatActivity {
-
+    Button btnMess;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
