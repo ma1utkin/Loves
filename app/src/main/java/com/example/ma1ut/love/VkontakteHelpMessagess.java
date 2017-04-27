1c50681cadb6182c25bbf0e9cf82c4a2710b0a82
ma1utkin <ma1utko94@mail.ru>
2017-04-27 20:13:19 +0300

    update

--- app/src/main/java/com/example/ma1ut/love/VkontakteHelpMessagess.java
+++ app/src/main/java/com/example/ma1ut/love/VkontakteHelpMessagess.java
@@ -1,12 +1,40 @@
 package com.example.ma1ut.love;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
+
 
 public class VkontakteHelpMessagess extends AppCompatActivity {
+    private String[] scope = new String[]{VKScope.MESSAGES, VKScope.FRIENDS, VKScope.WALL};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vkontakte_help_messages);
+        VKSdk.login(this,scope);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Вы успешно вошли", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+            @Override
+            public void onError(VKError error) {
+                // Произошла ошибка авторизации (например, пользователь запретил авторизацию)
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
---
+++ app/src/main/java/com/example/ma1ut/love/coreVkontakte/Application.java
@@ -0,0 +1,36 @@
+package com.example.ma1ut.love.coreVkontakte;
+
+import android.content.Intent;
+
+import com.example.ma1ut.love.VkontakteHelpMessagess;
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKAccessTokenTracker;
+import com.vk.sdk.VKSdk;
+
+/**
+ * Created by ma1ut on 27.04.2017.
+ */
+
+public class Application extends android.app.Application {
+
+    VKAccessTokenTracker vkAccessTokenTracker = new VKAccessTokenTracker() {
+        @Override
+        public void onVKAccessTokenChanged(VKAccessToken oldToken, VKAccessToken newToken) {
+            if (newToken == null) {
+                Intent intent = new Intent(Application.this, VkontakteHelpMessagess.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        }
+
+
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        vkAccessTokenTracker.startTracking();
+        VKSdk.initialize(this);
+    }
+}
