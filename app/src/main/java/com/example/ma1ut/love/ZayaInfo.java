ca647c0daf213550d1c661ac730e33e5037950c3
ma1utkin <ma1utko94@mail.ru>
2017-04-27 18:14:45 +0300

    update

--- app/src/main/java/com/example/ma1ut/love/ZayaInfo.java
+++ app/src/main/java/com/example/ma1ut/love/ZayaInfo.java
@@ -1,13 +1,46 @@
 package com.example.ma1ut.love;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ZayaInfo extends AppCompatActivity {
+public class ZayaInfo extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnMessages;
+    Button btnVk;
+    Button btnCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zaya_info);
+
+        btnMessages = (Button)findViewById(R.id.btnMessages);
+        btnVk = (Button)findViewById(R.id.btnVk);
+        btnCall = (Button)findViewById(R.id.btnCall);
+
+        btnMessages.setOnClickListener(this);
+        btnVk.setOnClickListener(this);
+        btnCall.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()){
+            case R.id.btnMessages:
+                intent = new Intent(this, MessagesActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.btnVk:
+                break;
+
+            case R.id.btnCall:
+                break;
+
+        }
     }
 }
