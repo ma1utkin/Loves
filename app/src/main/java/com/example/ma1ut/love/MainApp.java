7b3a7729187bb1859a673a3adfbdd3130dc054a6
ma1utkin <ma1utko94@mail.ru>
2017-04-28 07:58:34 +0300

    update

--- app/src/main/java/com/example/ma1ut/love/MainApp.java
+++ app/src/main/java/com/example/ma1ut/love/MainApp.java
@@ -1,10 +1,14 @@
 package com.example.ma1ut.love;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.vk.sdk.util.VKUtil;
 
@@ -14,11 +18,15 @@ public class MainApp extends AppCompatActivity {
 
 
     Button btnOk;
+    AlertDialog.Builder ad;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_app);
+        //Вызываем диалог с принятием правил
+        alertDia();
         String[] fingerprints = VKUtil.getCertificateFingerprint(this, this.getPackageName());
         System.out.println(Arrays.asList(fingerprints));
 
@@ -29,6 +37,25 @@ public class MainApp extends AppCompatActivity {
     public void btnOkMainAppClick(View view){
         Intent intent = new Intent(this, ZayaInfo.class);
         startActivity(intent);
+    }
+
+    public void alertDia(){
+        context = MainApp.this;
+        String title = "Внимание: ";
+        String message = "Приложение находиться в тестируемой версии, возможно проблемы с визуализацией Элементов экранов";
+        String button1String = "Ок, понятно";
+
+        ad = new AlertDialog.Builder(context);
+        ad.setTitle(title);
+        ad.setMessage(message);
+        ad.setPositiveButton(button1String, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String info = "Спасибо за понимание, а теперь воспользуйтесь нашими услугами";
+                Toast.makeText(getApplicationContext(),info,Toast.LENGTH_LONG).show();
+            }
+        });
+        ad.show();
     }
 
 }
